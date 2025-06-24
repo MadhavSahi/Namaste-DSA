a9fa01bc081c670f16a0671f595c0f172e81e1ba
Madhav Sahi <madhavsahi16@gmail.com>
2025-06-24 23:56:18 +0530

    added strings normal

---
+++ strings/normal/LC_125-ValidPalindrome.js
@@ -0,0 +1,22 @@
+var isPalindrome = function (s) {
+    let left=0;
+    let right=s.length-1;
+    while(left<=right){
+        if((s[left].toLowerCase()<"a" && s[left].toLowerCase()>"z") || (s[left].toLowerCase()<"0" && s[left].toLowerCase()>"9")){
+            left++;
+            // continue;
+        }
+        if((s[right].toLowerCase()<"a" && s[right].toLowerCase()>"z") || (s[right].toLowerCase()<"0" && s[right].toLowerCase()>"9")){
+            right--;
+            continue;
+        }
+        if(s[left]!==s[right]){
+            return false;
+        }
+        else{
+            left++;
+            right--;
+        }
+    }
+    return true;
+};
